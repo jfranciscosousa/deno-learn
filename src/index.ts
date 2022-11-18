77a7b9a3c30a7a680b899a91c900f4c72af18e25
Francisco Sousa <francisco.sousa@hey.com>
2022-11-18 21:13:21 +0000

    CORS

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import { Application } from "oak";
 import router from "./router.ts";
 
 const app = new Application();
+app.use((ctx, next) => {
+  ctx.response.headers.set("Access-Control-Allow-Origin", "*");
+
+  return next();
+});
 app.use(router.routes());
 app.use(router.allowedMethods());
 
